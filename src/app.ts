dc927daabd27afadea076ab7ff1bd38e53b2fa33
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-05-30 23:34:58 +0600

    change in repo

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
-
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 const app: Application = express()
 // const port = 3000
-app.use(cors());
+app.use(cors())
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //testing
 app.get('/', async (req: Request, res: Response) => {
-    res.send('Working Successfully')
+  res.send('Working Successfully')
 })
 
-export default app;
-
+export default app
