ecf20cf1ab2a1423b58dedce91b1be2c9df22d42
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-06-01 10:31:41 +0600

    cahgen in repo in 2nd imes

--- src/app/modules/users/users.route.ts
+++ src/app/modules/users/users.route.ts
@@ -1,15 +1,6 @@
-// import express from 'express'
-// import usersController from './users.controller'
-
-// const router = express.Router()
-// router.post('/create-user', usersController.creatUser)
-
-// export default router
-
 import express from 'express'
 import usersController from './users.controller'
 const router = express.Router()
-// router.post('/create-user', usersController.creatUser)
 router.post('/create-user', usersController.createUser)
 
 export default router
