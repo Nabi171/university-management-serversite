b482d736278451ab0619f9946d62719b360de8b4
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-06-10 15:48:31 +0600

    c i in the change of dealu in choose in whose change two side will be changed in data for the ppopularity in the skib in sklk shich whcik kike lks

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,10 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-// import userService from './app/modules/users/user.service'
-// import usersRouter from './app/modules/users/user.route'
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
-// import { UserRoutes } from './app/modules/users/user.route'
 import { UserService } from './app/modules/users/user.service';
 
-// import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route'
 const app: Application = express();
-// const port = 3000
 app.use(cors());
 //parser
 app.use(express.json());
@@ -17,17 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 
 //Application routes
 
-// app.use('/api/v1/users/', UserRoutes)
-// app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes)
 app.use('/api/v1/', routes);
 
-// //testing
-// app.get('/', (req: Request, res: Response,next:NextFunction) => {
-// throw new Error("Testing error logger")
-// // Promise.reject(new Error('Unhandled Promise Rejection'))
-// // next('Ore baba error')
-// })
-//global err handeling
 app.use(globalErrorHandler);
 app.get('/', async (req: Request, res: Response) => {
   await UserService.createUser({
