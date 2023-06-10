d59fba1510427ae7325004f58a4e38cfd79f3cb8
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-06-10 14:57:10 +0600

    c in the this code which ichange in theis repo which is existed to this backendnd code int thisn repo sti

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -11,8 +11,7 @@ import httpStatus from 'http-status';
 // const createUser:RequestHandler = async (req, res,next) => {
 //   try {
 
-//     const { user } = re
-
+//     const { user } = re\
 const createUser: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { user } = req.body;
