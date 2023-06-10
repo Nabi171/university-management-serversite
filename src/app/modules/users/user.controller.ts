4d7a9e04aa81b87e4a41afcc54735c96a162635d
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-06-10 14:13:11 +0600

    cahn heree change ain

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -15,7 +15,7 @@ import httpStatus from 'http-status';
 //     const result = await UserService.createUser(user)
 //     res.status(200).json({
 //       success: true,
-//       message: 'user created successfully',
+//       message: 'user created success
 
 const createUser: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
