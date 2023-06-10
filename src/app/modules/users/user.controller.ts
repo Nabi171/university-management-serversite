27e276f6ee0873a6372a1f2074aae7877628eccb
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-06-10 14:04:46 +0600

    cahnge the rin

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -20,7 +20,7 @@ import httpStatus from 'http-status';
 //     })
 //   } catch (err) {
 
-//     next
+//     nex
 
 const createUser: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
