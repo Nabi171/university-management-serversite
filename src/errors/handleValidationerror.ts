a9cb55f7ecd0942c4338b641477b9f25706fcd1a
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-06-10 12:20:15 +0600

    cahnge the repo again

--- src/errors/handleValidationerror.ts
+++ src/errors/handleValidationerror.ts
@@ -2,21 +2,6 @@ import mongoose from 'mongoose';
 import { IGenericErrorResponse } from '../interfaces/common';
 import { IGenericErrorMessage } from '../interfaces/error';
 
-// const handleValidationerror = (err: mongoose.Error.ValidationError):IGenericErrorResponse  => {
-//     const errors: IGenericErrorMessage[] = Object.values(err.errors).map(el: mongoose.Error.ValidationError | mongoose.Error.CastError)=> {
-//         return {
-//     path: el ?.path,
-//     message: el ?.message,
-// }
-//     }
-// )
-// const statusCode=400
-// return{
-
-// }
-
-// }
-
 const handleValidationerror = (
   error: mongoose.Error.ValidationError
 ): IGenericErrorResponse => {
