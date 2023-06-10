7d2a5487ee3fe313ec8207a7e7a890d2ffcd4aa2
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-06-10 14:15:01 +0600

    cahn heree chae

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -15,7 +15,7 @@ import httpStatus from 'http-status';
 //     const result = await UserService.createUser(user)
 //     res.status(200).json({
 //       success: true,
-//       message: 'user cre
+//       message: 'user
 
 const createUser: RequestHandler = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
