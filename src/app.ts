395f30cd9b0f3d40f260011dd12ddc5127ed1aed
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-06-11 01:14:32 +0600

    added in repo

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
-import { UserService } from './app/modules/users/user.service';
+import httpStatus from 'http-status';
+// import { UserService } from './app/modules/users/user.service';
 
 const app: Application = express();
 app.use(cors());
@@ -15,13 +16,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/', routes);
 
 app.use(globalErrorHandler);
-app.get('/', async (req: Request, res: Response) => {
-  await UserService.createUser({
-    id: '999',
-    password: '1234',
-    role: 'student',
+// app.get('/', async (req: Request, res: Response) => {
+//   await UserService.createUser({
+//     id: '999',
+//     password: '1234',
+//     role: 'student',
+//   });
+//   res.send('Working Successfully');
+// });
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
   });
-  res.send('Working Successfully');
+  next();
 });
 
 export default app;
