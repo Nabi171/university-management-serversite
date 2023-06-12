0f8d9caf49461a137bf38cb2e6d3933f8b946ae3
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-06-12 19:12:13 +0600

    changes in agin times

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -8,30 +8,29 @@ import catchAsync from '../../../shared/catchAsync';
 import pick from '../../../shared/pick';
 import { paginationFields } from '../../../constants/pagination';
 
-const createSemester = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { ...academicSemesterData } = req.body;
-    const result = await AcademicSemesterService.createSemester(
-      academicSemesterData
-    );
+const createSemester = catchAsync(async (req: Request, res: Response) => {
+  const { ...academicSemesterData } = req.body;
+  const result = await AcademicSemesterService.createSemester(
+    academicSemesterData
+  );
 
-    sendResponse<IAcademicSemester[]>(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Aademic semester created successfully!',
-      meta: result.meta,
-      data: result.data,
-    });
-    next();
-  }
-);
+  sendResponse<IAcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Aademic semester created successfully!',
+    data: result,
+  });
+});
 
 const getAllSemesters = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
+    const filters = pick(req.query, ['searchTerm', 'title', 'code', 'year']);
     const paginationOptions = pick(req.query, paginationFields);
-    console.log(paginationOptions);
+
+    console.log(filters);
 
     const result = await AcademicSemesterService.getAllSemesters(
+      filters,
       paginationOptions
     );
 
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,10 @@
 import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
 import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
-import { IAcademicSemester } from './academicSemester.interface';
+import {
+  IAcademicSemester,
+  IAcademicSemesterFilters,
+} from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 import { IPaginationOptions } from '../../../interfaces/pagination';
 import { IGenericResponse } from '../../../interfaces/common';
@@ -19,8 +22,59 @@ const createSemester = async (
 };
 
 const getAllSemesters = async (
+  filters: IAcademicSemesterFilters,
   paginationOptions: IPaginationOptions
 ): Promise<IGenericResponse<IAcademicSemester[]>> => {
+  const { searchTerm, ...filtersData } = filters;
+
+  const academicSemesterSearchableFields = ['title', 'code', 'year'];
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      $or: academicSemesterSearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
+
+  console.log(Object.entries(filtersData));
+  //  const andConditions = [
+  //     {
+  //       $or: [
+  //         {
+  //           title: {
+  //             $regex: searchTerm,
+  //             $options: 'i',
+  //           },
+  //         },
+  //         {
+  //           code: {
+  //             $regex: searchTerm,
+  //             $options: 'i',
+  //           },
+  //         },
+  //         {
+  //           year: {
+  //             $regex: searchTerm,
+  //             $options: 'i',
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ];
+
   const { page, limit, skip, sortBy, sortOrder } =
     paginationHelpers.calculatePagianation(paginationOptions);
 
@@ -33,7 +87,7 @@ const getAllSemesters = async (
   // const {page=1,limit=10}=paginationOptions;
   // const skip=(page-1)*limit;
 
-  const result = await AcademicSemester.find()
+  const result = await AcademicSemester.find({ $and: andConditions })
     .sort(sortConditions)
     .skip(skip)
     .limit(limit);
